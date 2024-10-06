e2f4dc620432dc8e81a5c1fc928c21cffc16c6c6
dev22 <ysoli36512@gmail.com>
2024-10-07 06:33:02 +0900

    2024-10-07 shift : Remove data from front

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -39,7 +39,16 @@ class SinglyLinkedList{
             this.tail = null;
         }
         return current;
-
+    }
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
     }
 }
 
